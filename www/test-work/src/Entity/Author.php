773291ba9ca9caf555c7919f0950048d1b6b1443
D.Pavlenko <dmitry.n.pavlenko@gmail.com>
2021-08-02 23:41:28 +0300

    Entities

---
+++ www/test-work/src/Entity/Author.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AuthorRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Contract\Entity\TranslatableInterface;
+use Knp\DoctrineBehaviors\Model\Translatable\TranslatableTrait;
+
+
+/**
+ * @ORM\Entity(repositoryClass=AuthorRepository::class)
+ */
+class Author implements TranslatableInterface
+{
+    use TranslatableTrait;
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Book::class, inversedBy="author")
+     */
+    private $book;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getBook(): ?Book
+    {
+        return $this->book;
+    }
+
+    public function setBook(?Book $book): self
+    {
+        $this->book = $book;
+
+        return $this;
+    }
+}
---
+++ www/test-work/src/Entity/Book.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use Knp\DoctrineBehaviors\Contract\Entity\TranslatableInterface;
+use Knp\DoctrineBehaviors\Model\Translatable\TranslatableTrait;
+
+
+/**
+ * @ORM\Entity(repositoryClass=BookRepository::class)
+ */
+class Book implements TranslatableInterface
+{
+    use TranslatableTrait;
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+
+    /**
+     * @ORM\OneToMany(targetEntity=Author::class, mappedBy="book")
+     */
+    private $author;
+
+    public function __construct()
+    {
+        $this->author = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Collection|Author[]
+     */
+    public function getAuthor(): Collection
+    {
+        return $this->author;
+    }
+
+    public function addAuthor(Author $author): self
+    {
+        if (!$this->author->contains($author)) {
+            $this->author[] = $author;
+            $author->setBook($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAuthor(Author $author): self
+    {
+        if ($this->author->removeElement($author)) {
+            // set the owning side to null (unless already changed)
+            if ($author->getBook() === $this) {
+                $author->setBook(null);
+            }
+        }
+
+        return $this;
+    }
+}
